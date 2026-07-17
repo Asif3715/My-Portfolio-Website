05feba53e645ce07d89ea5ebad9a86fd09456f2e
Asif Khan <tanoli3715@gmail.com>
2026-07-17 16:32:50 +0500

    feat: add Home link to navbar, set black text color, update profile image

--- app/_layout/navbar/index.jsx
+++ app/_layout/navbar/index.jsx
@@ -1,21 +1,12 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
-
 import { NavbarBrand } from './brand';
 import { NavbarList } from './list';
 
 export function Navbar() {
-  const pathname = usePathname();
-  const isHome = pathname === '/';
-
   return (
     <nav className='absolute inset-x-0 top-0 z-10'>
-      <div
-        className={`flex items-center justify-between px-8 py-4 ${
-          isHome ? 'text-background' : 'text-foreground'
-        }`}
-      >
+      <div className='flex items-center justify-between px-8 py-4 text-black'>
         <NavbarBrand />
         <NavbarList />
       </div>
