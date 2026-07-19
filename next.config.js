ce27a3c5553f8ee9edbe85f780ffbe0750265d91
Asif Khan <tanoli3715@gmail.com>
2026-07-19 20:02:26 +0500

    chore: update lint script, add format/stylelint scripts, and configure allowedDevOrigins

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
+const os = require('os');
+
+function getLocalIPs() {
+  const interfaces = os.networkInterfaces();
+  const ips = [];
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        ips.push(iface.address);
+      }
+    }
+  }
+  return ips;
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: [
+    'localhost',
+    '127.0.0.1',
+    '*.local',
+    ...getLocalIPs(),
+  ],
   images: {
     remotePatterns: [
       {
